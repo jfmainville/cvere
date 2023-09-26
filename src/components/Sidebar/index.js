8732bdf04658876357949e065087e79817e53abf
Jean-Frederic Mainville <jfmainville@outlook.com>
2023-09-25 21:23:50 -0400

    style: update the styling for the Sidebar component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./index.module.scss";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
     return (
         <div className={classes.Sidebar}>
-            Sidebar
+            <NavLink to={"/"} className={({isActive}) => (isActive ? classes.ActiveLink : classes.Link)}>Dashboard</NavLink>
+            <NavLink to={"/"} className={({isActive}) => (isActive ? classes.ActiveLink : classes.Link)}>Reports</NavLink>
         </div>
     );
 };
