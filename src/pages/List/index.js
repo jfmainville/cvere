04927f5e92caca3fc55d0d52906814b33d811383
Jean-Frederic Mainville <jfmainville@outlook.com>
2023-09-25 20:15:05 -0400

    refactor: update the fetchCVEData function to use dynamic date definitions

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -14,6 +14,10 @@ const List = () => {
     const fetchCVEData = async () => {
         const response = await axios.get("https://services.nvd.nist.gov/rest/json/cves/2.0?lastModStartDate=2023-09-18T13:00:00.000%2B01:00&lastModEndDate=2023-09-19T13:36:00.000%2B01:00")
         const vulnerabilities = response.data["vulnerabilities"]
+        let today = new Date()
+        let maxDate = new Date()
+        maxDate.setDate(today.getDate() - 30)
+        const response = await axios.get(`https://services.nvd.nist.gov/rest/json/cves/2.0?lastModStartDate=${maxDate.toISOString().replace("Z", "")}%2B01:00&lastModEndDate=${today.toISOString().replace("Z", "")}%2B01:00`)
         setCveList(vulnerabilities)
     }
 
