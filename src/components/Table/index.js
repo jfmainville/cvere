06e56c3a6d0438e16e60996e108f5ec90caeead7
Jean-Frederic Mainville <jfmainville@outlook.com>
2023-10-04 21:46:17 -0400

    refactor: update the CVEs table to use the ReactDataGrid component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,28 +1,20 @@
 import React from 'react';
+import ReactDataGrid from '@inovua/reactdatagrid-community';
+import '@inovua/reactdatagrid-community/index.css'
 import classes from "./index.module.scss";
 
-const Table = ({ cveTableHeaders, cveTableData }) => {
+const Table = ({ cveTableHeaders, cveTableData, defaultFilterValue }) => {
+    const gridStyle = { minHeight: 550 }
     return (
-        <table className={classes.Container}>
-            <thead>
-            <tr>
-                {cveTableHeaders.map((header) => (
-                    <th key={header}>{header}</th>
-                ))}
-            </tr>
-            </thead>
-            <tbody>
-            {cveTableData.map((cveItem) => (
-                <tr key={cveItem.id}>
-                    <td>{cveItem.id}</td>
-                    <td>{cveItem.description}</td>
-                    <td>{cveItem.vulnerabilityStatus}</td>
-                    <td>{cveItem.publishedDate}</td>
-                    <td>{cveItem.lastModifiedDate}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+        <ReactDataGrid
+            idProperty="cve_id"
+            columns={cveTableHeaders}
+            dataSource={cveTableData}
+            style={gridStyle}
+            className={classes.Container}
+            enableFiltering={true}
+            defaultFilterValue={defaultFilterValue}
+        />
     )
 };
 
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -4,7 +4,81 @@ import Table from "../../components/Table";
 import Spinner from "../../components/Spinner";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCVEs } from "../../store/cveActions";
+import SelectFilter from "@inovua/reactdatagrid-community/SelectFilter";
 
+const columns = [
+    {
+        name: "id",
+        header: "CVE Id",
+        defaultFlex: 1,
+        minWidth: 100,
+    },
+    {
+        name: "description",
+        header: "CVE Description",
+        defaultFlex: 2,
+        minWidth: 500,
+        filterEditor: SelectFilter,
+        filterEditorProps: {
+            multiple: true,
+            wrapMultiple: false,
+            dataSource: ['Google Workspace', 'Google Cloud Platform'].map(c => {
+                return { id: c, label: c }
+            }),
+        }
+    },
+    {
+        name: "vulnerabilityStatus",
+        header: "Vulnerability Status",
+        defaultFlex: 1,
+        minWidth: 100
+    },
+    {
+        name: "publishedDate",
+        header: "Published Date",
+        defaultFlex: 1,
+        minWidth: 200
+    },
+    {
+        name: "lastModifiedDate",
+        header: "Last Modified Date",
+        defaultFlex: 1,
+        minWidth: 200
+    }
+]
+
+const defaultFilterValue = [
+    {
+        name: 'id',
+        operator: 'contains',
+        type: 'string',
+        value: ''
+    },
+    {
+        name: 'description',
+        operator: 'inlist',
+        type: 'select', value: []
+    },
+       {
+        name: 'vulnerabilityStatus',
+        operator: 'contains',
+        type: 'string',
+        value: ''
+    },
+        {
+        name: 'publishedDate',
+        operator: 'date',
+        type: 'string',
+        value: ''
+    },
+        {
+        name: 'lastModifiedDate',
+        operator: 'date',
+        type: 'string',
+        value: ''
+    },
+
+];
 const List = () => {
     const cves = useSelector(state => state.cve.cves);
     const dispatch = useDispatch()
@@ -21,11 +95,12 @@ const List = () => {
     }
 
     return (<Layout>
-            <Table
-                cveTableHeaders={["CVE Id", "CVE Description", "Vulnerability Status", "Published Date", "Last Modified Date"]}
-                cveTableData={cves}
-            />
-        </Layout>);
+        <Table
+            cveTableHeaders={columns}
+            cveTableData={cves}
+            defaultFilterValue={defaultFilterValue}
+        />
+    </Layout>);
 }
 
 export default List;
