e0c4d7ce118c290d6b922f45d1ed25947f2912e0
Jean-Frederic Mainville <jfmainville@outlook.com>
2023-10-07 09:14:19 -0400

    refactor: rename the fetchCVEs parameters to startDate and endDate

--- src/store/cveActions.js
+++ src/store/cveActions.js
@@ -1,10 +1,10 @@
 import {cveActions} from "./cveSlice";
 import axios from "axios";
 
-export const fetchCVEs = (lastModifiedStartDate, lastModifiedEndDate) => {
+export const fetchCVEs = (startDate, endDate) => {
     return async (dispatch) => {
         const fetchData = async () => {
-            const response = await axios.get(`https://services.nvd.nist.gov/rest/json/cves/2.0?lastModStartDate=${lastModifiedStartDate.toISOString().replace("Z", "")}%2B01:00&lastModEndDate=${lastModifiedEndDate.toISOString().replace("Z", "")}%2B01:00`);
+            const response = await axios.get(`https://services.nvd.nist.gov/rest/json/cves/2.0?lastModStartDate=${startDate.replace("Z", "")}%2B01:00&lastModEndDate=${endDate.replace("Z", "")}%2B01:00`);
 
             if (response.status !== 200) {
                 throw new Error("Could not fetch CVE data");
