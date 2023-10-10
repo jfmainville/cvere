b5a9d1b227cce9ab9b16b0d799650d4d2491c392
Jean-Frederic Mainville <jfmainville@outlook.com>
2023-10-09 20:39:35 -0400

    refactor: remove the response.status condition
    Remove the response.status condition as errors always fall into the catch statement.

--- src/store/cveActions.js
+++ src/store/cveActions.js
@@ -5,11 +5,6 @@ export const fetchCVEs = (startDate, endDate) => {
     return async (dispatch) => {
         const fetchData = async () => {
             const response = await axios.get(`https://services.nvd.nist.gov/rest/json/cves/2.0?lastModStartDate=${startDate.replace("Z", "")}%2B01:00&lastModEndDate=${endDate.replace("Z", "")}%2B01:00`);
-
-            if (response.status !== 200) {
-                throw new Error("Could not fetch CVE data");
-            }
-
             return await response.data;
         };
 
