5bbbb0d00a80c29609b74ae3369ffa2208c30019
Jean-Frederic Mainville <jfmainville@outlook.com>
2023-10-09 20:42:40 -0400

    refactor: add the useEffect function to the Dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from "../../hoc/Layout";
 import VerticalBar from "../../components/Charts/VerticalBar";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./index.module.scss";
 import DoughnutBar from "../../components/Charts/DoughnutBar";
+import { fetchCVEs } from "../../store/cveActions";
+import Spinner from "../../components/Spinner";
 
 const Dashboard = () => {
-    const cves = useSelector(state => state.cve.cves)
+    const startDate = useSelector(state => state.cve.startDate);
+    const endDate = useSelector(state => state.cve.endDate)
+    const cves = useSelector(state => state.cve.cves);
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchCVEs(startDate, endDate))
+    }, [startDate, endDate]);
+
+    if (!cves) {
+        return (<Spinner/>)
+    }
 
     return (
         <Layout>
